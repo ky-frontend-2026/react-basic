fc0e189983053cecd08016962dcbd685681daafa
lizb <loo4love90@gmail.com>
2026-02-02 11:34:48 +0900

    basic

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import Aside from './components/Aside'
+function App() {
+
+
+  return (
+    <div className='container'>
+      <Header/>
+      <Aside/>
+      <h2>hello react!!!!</h2>
+    </div>
+
+  )
+}
+
+export default App
