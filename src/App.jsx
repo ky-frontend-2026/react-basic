dc1dc2bebcc26c055f15237cbc253f3e5d24056e
lizb <loo4love90@gmail.com>
2026-02-05 11:37:25 +0900

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Counter from './components/Counter'
 import Exam1 from './components/Exam1'
 import Exam2 from './components/Exam2'
 import Exam3 from './components/Exam3'
+import InputFocus from './components/InputFocus'
+import Counter2 from './components/Counter2'
 const App = () => {
   const user = {
     name: 'Alice',
@@ -10,9 +12,11 @@ const App = () => {
   }
   return (
     <div>
-      <h2>useState 연습</h2>
-      <Exam3 />
+      <h2>useState ,useRef연습</h2>
+      <Counter2/>
       <hr/>
+      <InputFocus/>
+      <Exam3 />
       <Exam2 />
       <Exam1 {...user}/>
       <Counter />
